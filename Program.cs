1803a6b684fb7f95fefc1482442811eded720ea8
vladislavtsitrikov <vladislavtsitrikov@yandex.ru>
2022-08-06 02:08:08 +0300

    Fix for use on victus laptop with new build of event utility

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Management;
 using System.Runtime.InteropServices;
-using System.ServiceProcess;
 using System.Threading;
 
-namespace RemapOMENKey {
-	class Program {
+namespace RemapOMENKey
+{
+	class Program
+	{
 		static void Main() => new Program().Run();
 
-		struct KEYBDINPUT {
+		struct KEYBDINPUT
+		{
 			public uint type;
 			public ushort wVk;
 			public ushort wScan;
@@ -20,20 +21,20 @@ namespace RemapOMENKey {
 			public uint unused1;
 			public uint unused2;
 		}
+
 		static readonly int SIZEOF_KEYBDINPUT = Marshal.SizeOf(typeof(KEYBDINPUT));
 
 		[DllImport("user32.dll", SetLastError = true)]
 		static extern uint SendInput(uint cInputs,
 			[MarshalAs(UnmanagedType.LPArray), In] KEYBDINPUT[] inputs, int cbSize);
+
 		[DllImport("user32.dll")]
 		static extern uint MapVirtualKey(uint uCode, uint uMapType);
 
 		readonly ManagementEventWatcher watcher = new ManagementEventWatcher(
-			new ManagementScope(@"\\localhost\root\wmi", null),
-			new EventQuery("select * from hpqbevnt"));
-		readonly ServiceController wmiSvc = new ServiceController("HPWMISVC");
-		readonly RegistryKey regKey = Registry.ClassesRoot.OpenSubKey(
-			@"CLSID\{60EB195D-B64E-4209-8AB8-53E040061B9C}\SystemEvent");
+			new ManagementScope("root\\WMI", null),
+			new EventQuery("SELECT * FROM hpqBEvnt")
+		);
 
 		const ushort VK_HOME = 0x24;
 
@@ -48,28 +49,38 @@ namespace RemapOMENKey {
 			new KEYBDINPUT(),
 		};
 
-		Program() {
-			homeInputs[0].wScan = (ushort) MapVirtualKey(VK_HOME, 0 /*MAPVK_VK_TO_VSC*/);
+		Program()
+		{
+			homeInputs[0].wScan = (ushort)MapVirtualKey(VK_HOME, 0 /*MAPVK_VK_TO_VSC*/);
 			homeInputs[1] = homeInputs[0];
 			homeInputs[1].dwFlags |= 2; // KEYEVENTF_KEYUP
 			watcher.EventArrived += EventArrived;
 		}
 
-		void Run() {
+		void Run()
+		{
 			watcher.Start();
+
+			foreach (Process p in Process.GetProcesses())
+			{
+				if (p.ProcessName.Equals("HP.Omen.OmenCommandCenter")) p.Kill();
+				if (p.ProcessName.Equals("OmenCommandCenterBackground")) p.Kill();
+			}
 			Thread.Sleep(Timeout.Infinite);
 		}
 
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031")]
-		void EventArrived(object sender, EventArrivedEventArgs e) {
-			try {
-				if (!4u.Equals(e.NewEvent["eventId"])) return;
-				wmiSvc.ExecuteCommand(0x83);
-				var buttonID = regKey.GetValue("ButtonID");
-				if (!8613.Equals(buttonID)) return;
+		void EventArrived(object sender, EventArrivedEventArgs e)
+		{
+			try
+			{
+				if (!((uint)29).Equals(e.NewEvent["eventId"])) return;
+				if (!((uint)8613).Equals(e.NewEvent["EventData"])) return;
 
-				SendInput((uint) homeInputs.Length, homeInputs, SIZEOF_KEYBDINPUT);
-			} catch (Exception exception) {
+				SendInput((uint)homeInputs.Length, homeInputs, SIZEOF_KEYBDINPUT);
+			}
+			catch (Exception exception)
+			{
 				Debug.WriteLine(exception);
 			}
 		}
